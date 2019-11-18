8a012e27e94a4928f409c000a899f47a5a84bdac
Günther Köckerandl <guenther.koeckerandl@sueddeutsche.de>
2019-11-18 18:18:37 +0100

    Implement HTTP signature

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const { Readable } = require('stream');
 const httpProxy = require('http-proxy');
+const CryptoJS = require('crypto-js');
 
 function createReadableStream(buffer) {
     const stream = new Readable();
@@ -14,13 +15,24 @@ function createReadableStream(buffer) {
 const proxy = httpProxy.createProxyServer({ws: false});
 
 proxy.on('proxyReq', function (proxyReq, req, res, options) {
-    proxyReq.setHeader('X-Signed-By', 'playground-signing-proxy');
+    const date = new Date().toUTCString();
+    const requestLine = `${req.method} ${req.rawPath} HTTP/1.1`;
+    const contentMD5 = CryptoJS.MD5(req.rawBody.toString()).toString(CryptoJS.enc.Base64);
+
+    const signingString = `date: ${date}\n${requestLine}\ncontent-md5: ${contentMD5}`;
+    const signature = CryptoJS.HmacSHA256(signingString, process.env.KEY_SECRET).toString(CryptoJS.enc.Base64);
+    const authorization = `Signature keyId="${process.env.KEY_ID}",algorithm="hmac-sha256",headers="date request-line content-md5",signature="${signature}"`;
+
+    proxyReq.setHeader('Date', date);
+    proxyReq.setHeader('Content-MD5', contentMD5);
+    proxyReq.setHeader('Authorization', authorization);
 });
 
 const server = require('http').createServer((req, res) => {
     req.rawBody = Buffer.alloc(0);
     const reqUrl = new URL(req.url);
     req.rawTarget = `${reqUrl.protocol || 'http:'}//${reqUrl.host}`;
+    req.rawPath = reqUrl.pathname;
 
     req.on('data', (data) => {
         req.rawBody = Buffer.concat([req.rawBody, data]);
